a6f418669fb18a9e4ce5d1a7e9c4032fca186224
Ribhu Lahiri <ribhu197@gmail.com>
2018-04-25 11:11:12 -0700

    Added connection to backend

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://ribhu:pass2905@ds014648.mlab.com:14648/persistent', (err, database) => {
+    if (err) return console.log(err)
+  const db = database;
+  console.log('database connected')
+});
+
 app.use(express.static(__dirname + '/views'));
 app.get('/', (req, res) => res.sendFile('/view/index.html'));
 
