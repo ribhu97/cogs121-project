0a1ed4e496fe82e10b55af7f798c815e7db8ef41
Alvin Vanegas <alvinvanegas@Alvins-MacBook-Pro-2.local>
2018-04-25 13:27:40 -0700

    using fakedatabase

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('views'));
+
+
+const fakeDatabase = {
+  'Model1': {attribute1: 'GPA', attribute2: 'Major', attribute3: 'Internships'}
+};
+
+
+app.get('/fakeDatabase/Model1', (req, res) => {
+  const model = fakeDatabase['Model1'];
+  console.log('Got data from fakeDatabase');
+
+});
+
+// start the server at URL: http://localhost:3000/
+app.listen(3000, () => {
+  console.log('Server started at http://localhost:3000/');
+});
