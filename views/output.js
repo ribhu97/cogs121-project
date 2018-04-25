309719123187be2aa6579c53a7bb6e95ddeeb431
Nikhil Bose <nikbose97@gmail.com>
2018-04-25 13:47:18 -0700

    typo fix

--- views/output.js
+++ views/output.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   			console.log('Got from Server - Client Side!');
   			$('#result-1').html("Attribute 1 is: " + data.attribute1);
   			$('#result-2').html("Attribute 2 is: " + data.attribute2);
-  			$('#result-3').html("Attribute 2 is: " + data.attribute3);
+  			$('#result-3').html("Attribute 3 is: " + data.attribute3);
   		});
 	
 	});
